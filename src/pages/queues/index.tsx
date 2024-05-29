005a414f0f47519c54f87fbf229cf52f9fa08fc3
angie pinilla <angiepinillanova@gmail.com>
2024-05-28 20:06:55 -0500

    feat: create queues in progress routes

---
+++ src/pages/queues/index.tsx
@@ -0,0 +1,7 @@
+import { QueuesUI } from "./interface";
+
+function Queues() {
+  return <QueuesUI />;
+}
+
+export { Queues };
---
+++ src/pages/queues/outlets/queuesInProgress/index.tsx
@@ -0,0 +1,7 @@
+function QueuesInProgress(){
+    return (
+        <></>
+    )
+}
+
+export { QueuesInProgress }
